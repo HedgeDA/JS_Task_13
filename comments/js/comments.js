50839347dd77297689aa75c168fd44354bfb8030
HedgeDA <faraon.da@gmail.com>
2019-02-10 19:02:38 +0600

    ...

--- comments/js/comments.js
+++ comments/js/comments.js
@@ -2,28 +2,59 @@
 
 function showComments(list) {
   const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+  const commentsNodes = list.map(createCommentNode);
+  const fragment = commentsNodes
+    .reduce((fragment, currentValue) => {
+      fragment.appendChild(currentValue);
+      return fragment;
+    }, document.createDocumentFragment());
+  commentsContainer.appendChild(fragment);
 }
 
-function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+function node(tagName, attributes = {}, textContent = '') {
+  const newNode = document.createElement(tagName);
+
+  for (let attribute in attributes) {
+    newNode[attribute] = attributes[attribute];
+  }
+
+  if (typeof textContent === 'string') {
+    if (/\n/.test(textContent)) {
+      textContent.split('\n').forEach(text => {
+        if (!text) {
+          newNode.appendChild(node('br'));
+        } else {
+          newNode.appendChild(node('p', {}, text));
+        }
+      })
+    } else {
+      newNode.textContent = textContent;
+    }
+  } else if (textContent instanceof Array) {
+    textContent.forEach(childNode =>
+      newNode.appendChild(childNode)
+    );
+  }
+
+  return newNode
+}
+
+function createCommentNode(comment) {
+  return node('div', {'className': 'comment-wrap'}, [
+    node('div', {'className': 'photo', 'title': comment.author.name}, [
+      node('div', {'className': 'avatar', 'style': `background-image: url('${comment.author.pic}')`})
+    ]),
+    node('div', {'className': 'comment-block'}, [
+      node('p', {'className': 'comment-text'}, comment.text),
+      node('div', {'className': 'bottom-comment'}, [
+        node('div', {'className': 'comment-date'}, new Date(comment.date).toLocaleString('ru-Ru')),
+        node('ul', {'className': 'comment-actions'}, [
+          node('li', {'className': 'complain'}, 'Пожаловаться'),
+          node('li', {'className': 'reply'}, 'Ответить')
+        ])
+      ])
+    ])
+  ]);
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
