cc550df44d9bb5f60b3b03566e8295f66f2039f3
HedgeDA <faraon.da@gmail.com>
2019-02-11 00:53:48 +0600

    ...

--- store/js/dom.js
+++ store/js/dom.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function createElement(content) {
+  if (typeof content === 'string') {
+    return document.createTextNode(content);
+  }
+
+  let element = document.createElement(content.name);
+
+  for (let property in content.props) {
+    element.setAttribute(property, content.props[property]);
+  }
+
+  content.childs.forEach(child => element.appendChild(createElement(child)));
+
+  return element;
+}
+
